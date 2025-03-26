9cbd58aa240d53294b42a023cf48f17f6e0d16e5
Oliver Blanthorn <freedom4cows@gmail.com>
2025-03-26 19:54:53 +0100

    add population graphs that really have no business being in this repo
    also update to h3 v0.3.0 which breaks the top half of the code

--- grid2h3.jl
+++ grid2h3.jl
@@ -1,3 +1,4 @@
+# nb: use julia +1.9.4 to use H3 v0.2.0
 using Shapefile, DataFrames, Statistics, RollingFunctions, Proj, ThreadsX, StatsBase, CSV
 import H3
 import H3.API: kRing, geoToH3, GeoCoord, h3ToParent, hexAreaKm2
@@ -171,8 +172,8 @@ write("../population-around-a-tile/map/data/JRC_POPULATION_2018_H3_by_rnd/meta.j
 # tiff2arrow
 using GeoArrays, Proj, Arrow, DataFrames
 mollweide2latlon = Proj.Transformation("ESRI:54009", "EPSG:4326", always_xy=true) # always_xy => lon/lat order
-ghs = GeoArrays.read("ghs/GHS_POP_E2020_GLOBE_R2023A_54009_100_V1_0.tif", masked=false) # masked=false memmaps it
-t = GeoArrays.coords(ghs, (100_000,100_000))
+# ghs = GeoArrays.read("ghs/GHS_POP_E2020_GLOBE_R2023A_54009_100_V1_0.tif", masked=false) # masked=false memmaps it
+ghs = GeoArrays.read("ghsl_1km/GHS_POP_E2025_GLOBE_R2023A_54009_1000_V1_0.tif", masked=false) # 1km rather than 100x100
 filter(i -> ghs[i] >= 0, Iterators.partition(eachindex(ghs), 1000*1000) |> first)
 
 #using ThreadsX
@@ -193,11 +194,17 @@ using Dates
     df = DataFrame(map(c -> (mollweide2latlon(GeoArrays.coords(this_ghs, c.I[1:2]))..., this_ghs[c]), nonzeroes), [:lon, :lat, :pop]) # about 100 seconds per coordinate partition
     Arrow.write("arrow_nocompress/part$i.arrow", df)#, compress=:zstd) # pretty sure compression means we can't memory map
 end
+# for 1km squares
+nonzeroes = findall(p -> p > 0.0, ghs) # exclude both no data and true zeroes 
+df = DataFrame(map(c -> (mollweide2latlon(GeoArrays.coords(ghs, c.I[1:2]))..., ghs[c]), nonzeroes), [:lon, :lat, :pop]) # about 100 seconds per coordinate partition
+Arrow.write("ghs_1km.arrow", df)#, compress=:zstd) # pretty sure compression means we can't memory map
 
 # gdal_translate -of XYZ GHS_POP_E2020_GLOBE_R2023A_54009_100_V1_0.tif ghs.csv # single threaded but better? who knows
 
 # gdal_translate -of XYZ GHS_POP_E2020_GLOBE_R2023A_54009_100_V1_0.tif /vsistdout | awk '$3 != -200' > ghs.csv # skip nodata,
 # absurdly slow (ran for four hours and got ~1/4 of the way through), julia does it in 20 minutes
+
+# https://human-settlement.emergency.copernicus.eu/download.php?ds=pop - no real reason why we have to do 100x100m grid to get an MVP out, start with 1km and it'll be digestable
 
 
 
@@ -325,3 +332,102 @@ writehivedir2("../population-around-a-tile/map/data/JRC_POPULATION_2018_H3_by_rn
 df = readhivedir("../population-around-a-tile/map/data/JRC_POPULATION_2018_H3_by_rnd/")
 df.h3_parent = df.h3_3
 writehivedir2("../population-around-a-tile/map/data/jrc2/", df[!, [:index, :value, :real_value, :population, :h3_parent, :res]], [:res, :h3_parent])
+
+
+# progress with GHS 1km^2
+# written to ghs_1km.arrow, 1km^2 centroids + population in that square
+# just need to rerun the shapefile stuff
+using Arrow
+df = Arrow.Table("ghs_1km.arrow") |> DataFrame
+df.id = 1:size(df, 1)
+mini_df = df
+ 
+rings = mapreduce(k -> map(midpoint -> (id=midpoint[3], h3=kRing(geoToH3(GeoCoord(deg2rad.(midpoint[1:2])...), 9), k), dist=k+1), zip(mini_df.lat,mini_df.lon, df.id)), vcat, 0:2) |> DataFrame
+rings = flatten(rings, :h3)
+
+# sort!(rings, :dist) # comes presorted
+rings = rings[.!nonunique(rings, [:h3, :id], keep=:first), :]
+leftjoin!(rings, mini_df[!, [:pop, :id]], on=:id)
+
+# weighted average with centre = 1, nthring = 1/dist^3
+df2 = combine(groupby(rings, :h3), [:dist, :pop] => ((d, p) -> mean(p, weights(1 ./ (d.^3)))) => :pop)
+df2.res .= 9
+df2.h3_3 = ThreadsX.map(h -> h3ToParent(h, 3), df2.h3)
+rename!(df2, :h3_3 => :h3_3i)
+rename!(df2, :pop => :value)
+Arrow.write("ghs_h3_9.arrow", df2)
+df2.h3_3 .= ThreadsX.map(h -> string(h, base=16), df2.h3_3i)
+transform!(groupby(df2, :h3_3i), :h3_3i => x -> string.(x, base=16) => :h3)
+# GC.gc()
+# df2.index = string.(df2.h3, base=16)
+using Arrow, DataFrames
+import H3.API: kRing, geoToH3, GeoCoord, h3ToParent, hexAreaKm2
+df = Arrow.Table("ghs_h3_9.arrow") |> DataFrame
+transform!(groupby(df, :h3_3i), :h3_3i => (x -> string(x[1], base=16)) => :h3_3)
+using ArrowHivePartitioner
+function writehivedir2(outdir, df, groupkeys=[]; filename="part0.arrow") # arquero doesn't support zstd
+    g = groupby(df, groupkeys)
+    for t in keys(g)
+        !all(v -> >:(AbstractString, typeof(v)), values(t)) && throw("All grouped column values must be strings") # TODO: support other types?
+        path = join(["$k=$v" for (k,v) in zip(keys(t), values(t))], "/")
+        mkpath(joinpath(outdir,path))
+        Arrow.write(joinpath(outdir,path,filename), g[t][!, Not(keys(t))])
+    end
+end
+writehivedir2("ghs_hive/", df[!, [:h3, :value, :res, :h3_3]], [:h3_3])
+
+# todo: rename h3_3 to h3_parent, res=9 to folder
+# convert h3 -> index string (will need to do one at a time really)
+# .. tbh probably actually better to do this for res=7, res=5 first? could even say "high res data only available for europe". total size would be 10GB otherwise, too big for github
+
+
+
+# can't remember any of this stuff
+using ArrowHivePartitioner
+using Arrow, DataFrames
+import H3, Plots, ThreadsX
+# import H3.API: kRing, geoToH3, GeoCoord, h3ToParent, hexAreaKm2, h3Distance, edgeLengthKm # eugh a version update changed all these
+import H3.API: latLngToCell, LatLng, hexAreaKm2, gridDistance, edgeLengthKm, gridDisk
+df = readhivedir("../population-around-a-tile/map/data/JRC_POPULATION_2018_H3_by_rnd/res=7/")
+df.h3 = parse.(UInt64, df.index, base=16)
+
+# [sum(df[in.(df.h3, Ref(kRing(df.h3[1],i))), :population]) for i in 1:10] # this is annoyingly slow, why is it so slow -> maybe better to do it with a distance thing first
+
+function popatdist(lonlatstr, distances=0:60)
+    # lonlatstr = "8.540368 47.378156"
+    lonlat = deg2rad.(parse.(Float64, split(lonlatstr)))
+    # distances = 0:10
+    # candidates = gridDisk(latLngToCell(LatLng(lonlat[2], lonlat[1]), 9), distances[end]) # in check against this seems to be too slow
+    df.dist = ThreadsX.map(h -> gridDistance(h, latLngToCell(LatLng(lonlat[2], lonlat[1]), 7)), df.h3)
+    # radius_mult = edgeLengthKm(9) # broken?
+    # radius_mult = 0.174375668 
+    radius_mult = 3 # guesstimate
+    # gridDistance(df.h3[end], df.h3[1]) # regression in H3. this used to be -1
+    df[(df.dist .== 0) .&& (df.h3 .!= df.h3[1]), :dist] .= -1
+    mini = copy(df[distances[1] .<= df.dist .<= distances[end], :])
+    sort!(mini, :dist)
+    mini.cumpop = cumsum(mini.population)
+    plt = mini[.!nonunique(mini, :dist, keep=:last), :]
+    return (radius_mult .* plt.dist, plt.cumpop ./ 1_000_000 )
+end
+# grab from https://epsg.io/map#srs=4326&x=4.900615&y=52.378937&z=15&layer=streets
+zurich = popatdist("8.540368 47.378156")
+p = Plots.plot(zurich..., label="Zurich", ylabel="Population (millions)", xlabel="Distance (km)")
+paris = popatdist("2.348928 48.853252")
+Plots.plot!(p, paris..., label="Paris")
+london = popatdist("-0.126368 51.532502")
+Plots.plot!(p, london..., label="London")
+munich = popatdist("11.557102 48.140284")
+Plots.plot!(p, munich..., label="Munich")
+brussels = popatdist("4.336029 50.835107")
+Plots.plot!(p, brussels..., label="Brussels")
+# amsterdam = popatdist("4.900615 52.378937")
+# Plots.plot!(p, amsterdam..., label="Amsterdam")
+bordeaux = popatdist("-0.590343 44.857816")
+Plots.plot!(p, bordeaux..., label="Bordeaux")
+# lyon = popatdist("4.860058 45.760454")
+# Plots.plot!(p, lyon..., label="Lyon")
+strasbourg = popatdist("7.734182 48.585306")
+Plots.plot!(p,strasbourg..., label="Strasbourg")
+manchester = popatdist("-2.229845 53.477218")
+Plots.plot!(p, manchester..., label="Manchester")
